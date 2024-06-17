6e9bd3a3405d964fcdb1e2ab25cb515a67decf63
Astroxzh <166394844+Astroxzh@users.noreply.github.com>
2024-06-17 16:37:40 +0200

    auto save_1

--- hmflux/gui/saveImageGUI.py
+++ hmflux/gui/saveImageGUI.py
@@ -11,7 +11,9 @@ from magicgui import magicgui
 class SaveImageGUI(BaseGUI):
     ''' main class to save image'''
 
-    DEFAULT = {'nameGUI': 'Save Image'}
+    DEFAULT = {'nameGUI': 'Save Image',
+               'autoGUI': 'Auto Save'
+               }
 
     def __init__(self, viscope, **kwargs):
         ''' initialise the class '''
@@ -33,9 +35,28 @@ class SaveImageGUI(BaseGUI):
             if idxIncrement:
                 saveGui.fileIdx.value = saveGui.fileIdx.value + 1 
 
+
+        @magicgui(filePath={"label": "Saving file Path:","mode":'d'},
+                fileName={"label": "Saving file Name:"},
+                fileIdx = {"label": "File Index:"},
+                slmX = {"widget_type": "FloatSpinBox","value":0,"step":0.01},
+                slmY = {"widget_type": "FloatSpinBox","value":0,"step":0.01},
+                slmZ = {"widget_type": "FloatSpinBox","value":0,"step":0.01})
+        def autoSaveGui(filePath= Path(self.viscope.dataFolder), fileName: str = 'Image', fileIdx=0,idxIncrement=True,slmX=0,slmY=0,slmZ=0):
+
+            np.save(str(filePath / fileName) + f'_{fileIdx}',self.device.rawImage)            
+
+            if idxIncrement:
+                autoSaveGui.fileIdx.value = saveGui.fileIdx.value + 1 
+
+
         # add widgets 
         self.saveGui = saveGui
         self.vWindow.addParameterGui(self.saveGui,name=self.DEFAULT['nameGUI'])
+        self.autoSaveGui = autoSaveGui
+        self.vWindow.addParameterGui(self.autoSaveGui,name=self.DEFAULT['autoGUI'])
+
+
  
 
 if __name__ == "__main__":
