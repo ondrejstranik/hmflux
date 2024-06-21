1399d8c7bf9c27fe88ce9ea990d0858f206efbfd
Astroxzh <166394844+Astroxzh@users.noreply.github.com>
2024-06-21 09:10:34 +0200

    auto save update_2

--- hmflux/autoSaveScript.py
+++ hmflux/autoSaveScript.py
@@ -1,4 +1,7 @@
+
+#%%
 import os
+from pathlib import Path
 
 from hmflux.instrument.camera.andorCamera.andorCamera import AndorCamera
 from hmflux.instrument.camera.avCamera.avCamera import AVCamera        
@@ -16,7 +19,7 @@ class AutoSave():
         self.camera.connect()
         self.camera.setParameter('exposureTime', 300)
         self.camera.setParameter('nFrame', 1)
-        self.camera.setParameter('threadingNow',True)
+        #self.camera.setParameter('threadingNow',True)
 
         # slm
         self.slm = ScreenSLM('slm')
@@ -26,27 +29,25 @@ class AutoSave():
         self.stage = SmarACTStage('stage')
         self.stage.connect()
         #all parameters
-        self.folderName = '20062024-1'
-        self.dataFolder = r'.\DATA'
+        self.folderName = '20240620-1'
+        self.dataFolder = r'.\hmflux\DATA'
         self.path = self.dataFolder+'./'+self.folderName
 
         #stage parameter
         self.stageX = 0
-        self.stageY = 0
+        self.stageY = 0.1
         self.stageZ = 0
-        self.stageMove = np.array((self.stageX,self.stageY,self.stageZ))
-        self.axis = ['X', 'Y', 'Z']
+        # self.stageMove = np.array((self.stageX,self.stageY,self.stageZ))
+        # self.axis = ['X', 'Y', 'Z']
 
         #camera image
         self.constantImage = []
         self.boxImage = []
-        self.numberOfImage = 10
+
 
         #slm parameter
-        sizeX = 200
-        sizeY = 100
         self.imageSLM = ImageSLM()
-        self.imageSLM.setSizeSLM(sizeX,sizeY)
+        self.imageSLM.setSizeSLM(self.slm.sizeX,self.slm.sizeY)
 
         self.constantValue = 0
         self.binaryValue0 = 0
@@ -62,7 +63,7 @@ class AutoSave():
 
         self.mkdir()
 
-    def mkdir(self,path):
+    def mkdir(self):
         folder = os.path.exists(self.path)
         if not folder:
             os.makedirs(self.path)
@@ -74,29 +75,27 @@ class AutoSave():
 
     def save(self):
         for i in range(len(self.constantImage)):
-            pathConstant = self.path+'./'+'Constant'
-            pathBox = self.path+'./'+'Box'
+            pathConstant = Path(self.path+'./'+'Constant')
+            pathBox = Path(self.path+'./'+'Box')
             fileName = 'Image'
-            fileIdx = 0
-            np.save(str(pathConstant / fileName) + f'_{fileIdx}',self.constantImage[i])
-            np.save(str(pathBox/ fileName) + f'_{fileIdx}',self.boxImage[i])
-            fileIdx = fileIdx + 1 
+            np.save(str(pathConstant / fileName) + f'_{i}',self.constantImage[i])
+            np.save(str(pathBox/ fileName) + f'_{i}',self.boxImage[i])
 
 
-    def record(self):
-        for i in range(self.numberOfImage):
+    def record(self,numberOfImage):
+        for i in range(numberOfImage):
             #constant
-            slmImage = np.zeros([self.sizeX,self.sizeY])
+            # slmImage = np.zeros([self.sizeX,self.sizeY])
+            print(f'recording {i} image')
             slmImage = self.imageSLM.generateConstant(self.constantValue)
             self.slm.setImage(slmImage)
             self.rawConstant = self.camera.getLastImage()
             self.constantImage.append(self.rawConstant)
             
             #box1
-            slmImage = np.zeros([self.sizeX,self.sizeY])
             slmImage = self.imageSLM.generateConstant(self.constantValue)
             slmImage += self.imageSLM.generateBinaryGrating(1-self.boxAxis,self.binaryValue0,self.binaryValue1)
-            slmImage = self.imageSLM.generateBox1(axis=self.boxAxis,
+            slmImage += self.imageSLM.generateBox1(axis=self.boxAxis,
                                             position=self.boxPosition,
                                             val0=self.boxValue0,
                                             val1=self.boxValue1,
@@ -106,23 +105,39 @@ class AutoSave():
             self.rawBox = self.camera.getLastImage()
             self.boxImage.append(self.rawBox)
 
-            self.stage.move(self.stageMove,self.axis)
+            if self.stageX != 0:
+                self.stage.move(self.stageX,'X')
+            elif self.stageY != 0:
+                self.stage.move(self.stageY,'Y')
+            elif self.stageZ != 0:
+                self.stage.move(self.stageZ,'Z')
 
-            if keyboard.is_pressed('q'):
+
+            if keyboard.is_pressed('p'):
                 print("Loop terminated")
-            break
+                break
 
-autoSave = AutoSave()
-autoSave.record()
-autoSave.save()
+#%%
+
+if __name__ == '__main__':
+    import napari
+
+    autoSave = AutoSave()
+    numberOfImage = 10
+    autoSave.record(numberOfImage)
+    print('record done')
+    autoSave.save()
+    print('save done')
 
 
+    autoSave.camera.disconnect()
+    autoSave.stage.disconnect()
+    autoSave.slm.disconnect()    
 
-
-
-autoSave.camera.disconnect()
-autoSave.stage.disconnect()
-autoSave.slm.disconnect()    
+    viewer = napari.Viewer()
+    viewer.add_image(np.array(autoSave.constantImage))
+    viewer.add_image(np.array(autoSave.boxImage))
+    napari.run()
 
 
 #%%
